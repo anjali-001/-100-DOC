e0d733178bbbcc1d39fc2e3773382e649cad7605
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-02-07 22:25:11 +0530

    Can't debug.

---
+++ Day31/helloworld/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter/>
+       {/* <Greet name="Lakshya" dept="ECE">
+         <p>This is children props.</p>
+       </Greet>
+       <Greet name="Akash" dept="Marine">
+         <button>Action</button>
+       </Greet>
+       <Greet name="Ahiri" dept="BT"/>
+       <Welcome name="Lakshya" dept="ECE"/>
+       <Welcome name="Akash" dept="Marine"/>
+       <Welcome name="Akash" dept="Marine"/> */}
+       {/* <Hello/> */}
+       {/* <Message/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
