10a5ef3f04bae69e1e2f09766790375c110634f5
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-02-22 20:00:54 +0530

    30.15 min

---
+++ reactpractice/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Practice1 from './Practice1'
+import MyInfo from './MyInfo';
+
+
+
+function App() {
+  return (
+
+    
+    <div className="App">
+    <Practice1/>
+    <MyInfo/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ reactpractice/src/Practice1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Practice1() {
+    return (
+        <div>
+            <ul>
+    <li>First</li>
+    <li>Second</li>
+    <li>Third</li>
+</ul>
+        </div>
+    )
+}
+
+export default Practice1
