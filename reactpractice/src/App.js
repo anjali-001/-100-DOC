9c853426ebd99ccde6b56633f78e6cd948bca453
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-02-24 14:30:34 +0530

    Day48

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -5,17 +5,23 @@ import Practice1 from './components/Practice1'
 import MyInfo from './components/MyInfo';
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent';
-import Footer from './Footer';
-import Joke from './Joke'
+import Footer from './components/Footer';
+import Joke from './components/Joke'
+import TodoItem from './components/TodoItem';
+import todosData from './components/todosData';
 
 
 
 function App() {
+  const TodoItems = todosData.map(item => <TodoItem key = {item.id} item={item}/>)
+
   return (
 
     
     <div className="App">
-      <Joke/>
+
+      {TodoItems}
+      {/* <Joke/> */}
 
     {/* <Practice1/>
     <MyInfo/>
