b05485795eead8d1825e239c9807dab8dcf910da
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-02-27 21:47:45 +0530

    Fetch API and Forms

---
+++ Day51/src/LoggingData.js
@@ -0,0 +1,18 @@
+const LoggingData = [
+    {
+        id: 1,
+        log: "In",
+        stat: true
+    },
+    {
+        id: 2,
+        log: "Out",
+        stat: false
+
+    }
+
+
+
+]
+
+export default LoggingData 
