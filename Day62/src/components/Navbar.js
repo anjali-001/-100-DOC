7b4e6f9a96a36727f84fd8a00eb91d57903dc2ae
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-03-09 14:43:43 +0530

    Context API Basics

--- Day62/src/components/Navbar.js
+++ Day62/src/components/Navbar.js
@@ -5,8 +5,8 @@ class Navbar extends Component {
     static contextType = ThemeContext
     render() {
         console.log(this.context);
-        const { isLigthTheme, light, dark} = this.context;
-        const theme = isLigthTheme? light : dark;
+        const { isLightTheme, light, dark} = this.context;
+        const theme = isLightTheme? light : dark;
         return (
             <nav style={{ background: theme.ui, color: theme.syntax}}>
                 <h1>Book List App</h1>
