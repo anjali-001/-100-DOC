067f4ffab6e37c37d71ae96a708f600f0c699ccc
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-03-13 15:14:12 +0530

    Day66: Context with Functional Components

--- contextapp(updated)/src/components/Booklist.js
+++ contextapp(updated)/src/components/Booklist.js
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
+import {BookContext} from '../contexts/BookContext';
 
 
 const Booklist = () => {
 
     const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const {books} = useContext(BookContext);
     const theme = isLightTheme? light : dark;
     return ( 
         <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
                 <ul>
-                    <li style={{background: theme.ui}}>Sita: Warrior if Mithila</li>
-                    <li style={{background: theme.ui}}>Murakaami</li>
-                    <li style={{background: theme.ui}}>1984</li>
+                    {books.map(book => {
+                        return(
+                            <li key= {book.id} style={{background: theme.ui}}>{book.title}</li>
+                        )
+                    })}
                 </ul>
             </div>
      );
