0acb95db3cf689b79e7bc8f778193401bfe673eb
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-03-25 20:15:44 +0530

    Day78: Updated Product.js

--- ecommerce/src/components/Product.js
+++ ecommerce/src/components/Product.js
@@ -13,17 +13,23 @@ export default class Product extends Component {
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3
             my-3">
                 <div className="card">
-                <div className="img-container p-5" onClick={() => console.log('you clicked me on the image container')}>
+                    <ProductConsumer>
+                      {(value) => (
+                      <div className="img-container p-5" onClick={() => value.handleDetail(id) }>
                    <Link to="/details">
                        <img src={img} alt="product" className="card-img-top"/>
                     </Link> 
                     <button className="cart-btn" disabled={inCart?true:false} onClick={()=>{
-                        console.log('added to the cart');
+                        value.addToCart(id);
+                        value.openModal(id);
                     }}>
                          {inCart? (<p className="text-capitalize mb-0" disabled>{" "}inCart</p>):(<i className="fas fa-cart-plus"/>)}  
                     </button>
                    
-                </div>
+                </div>)}  
+                    
+                
+                </ProductConsumer>
                     {/*card footer*/}
                     <div className="card-footer d-flex justify-content-between">
                     <p className="align-self-center mb-0">{title}</p>
