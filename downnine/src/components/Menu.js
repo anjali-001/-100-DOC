82b3cd49f128847df61551922b167c055a36e297
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-04 19:16:59 +0530

    Day88: Updated Menu.js

--- downnine/src/components/Menu.js
+++ downnine/src/components/Menu.js
@@ -1,23 +1,35 @@
 import React, {Component,useContext} from 'react';
 import {DataContext} from '../context';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
 
 const Menu = ({item}) => {
     const {handleDetail} = useContext(DataContext);
     const {id,img,price,title} = item
     return (
-    <div className="container">
-        <div className="row">
+    <MenuWrapper className="container  col-max-9 mx-auto col-md-6 col-lg-3" >
+        <div className="row ">
             <div className="card">
                         <div className="img-container p-5" onClick={()=>handleDetail(id)}>
                             <Link to="/details">
                                 <img src={img} alt='cocktail' className="card-img-top"/>
                             </Link>
+                            <div className="card-footer d-flex justify-content-between bg-dark text-light">
+                            <h5 className="mb-0"><span className="mr-1">$</span>
+    {price}</h5>
+    <p className="align-self-center mb-0">{title}</p>
+    <button className="cart-btn">
+        <Link to="/cart" className="cart-btn">
+        <i className="fas fa-cart-plus"/>
+        </Link>
+    </button>
+                            </div>
                         </div>
             </div>
         </div>
         
-    </div>
+    </MenuWrapper>
     )
 }
 
@@ -35,3 +47,36 @@ export default Menu
 
 // export default Menu
 
+
+const MenuWrapper = styled.div`
+.card{
+    border-color:transparent;
+}
+.card-footer{
+    background: var(--mainDark)!important;
+    color: var(--lightYellow)!important;
+}
+&:hover{
+    .card{
+        border:0.04rem solid rgb(230, 138, 0);
+    }
+}
+.img-container{
+    position: relative;
+    overflow:hidden;
+}
+.img-container:hover .card-img-top{
+    trasnform:scale(1.2);
+}
+
+.cart-btn{
+    background: var(--mainDark);
+    color: var(--lightYellow);
+    border-color: var(--lightYellow);
+
+}
+.cart-btn:hover {
+    color: var(--darkYellow);
+    border-color: var(--darkYellow);
+}
+`
