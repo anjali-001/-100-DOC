d65a45bb8f7ddc1a3273c08d6280a0fb58468276
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-05 15:41:33 +0530

    Day89: Updated Home.js

--- downnine/src/components/Home.js
+++ downnine/src/components/Home.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react';
+import {ButtonContainer} from './Button';
+import {DataContext} from '../context';
 
-export class Home extends Component {
-    render() {
-        return (
-            <div>
-                hello from home
+const Home = () => {
+    const {products,generateNum,num} = useContext(DataContext);
+    const {id} = products
+    
+    return ( 
+        <div>
+                <h4>Having trouble deciding your drink? Let your fate choose for you.</h4>
+                <p>Click below to see what's your cocktail.</p>
+                <ButtonContainer onClick={()=>generateNum()}>click{num}</ButtonContainer>
+                {()=>{
+                    {id}
+                }}
+
+                
             </div>
-        )
-    }
+     );
 }
-
-export default Home
+ 
+export default Home;
