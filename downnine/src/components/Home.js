b2fd34efd9fda4b3fdf50d51f4f8f4c1bb55b319
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-06 17:11:30 +0530

    Day90:Updated Home.js (Game)

--- downnine/src/components/Home.js
+++ downnine/src/components/Home.js
@@ -1,23 +1,56 @@
 import React, { useContext } from 'react';
 import {ButtonContainer} from './Button';
 import {DataContext} from '../context';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom'
 
 const Home = () => {
-    const {products,generateNum,num} = useContext(DataContext);
-    const {id} = products
+    const {generateTitle,randomTitle} = useContext(DataContext);
     
     return ( 
-        <div>
+        <HomeGameWrapper className="container col-lg-6 col-sm-10 text-center mx-auto" >
                 <h4>Having trouble deciding your drink? Let your fate choose for you.</h4>
                 <p>Click below to see what's your cocktail.</p>
-                <ButtonContainer onClick={()=>generateNum()}>click{num}</ButtonContainer>
-                {()=>{
-                    {id}
-                }}
+                <div>
+                <ButtonContainer onClick={()=>generateTitle()}>click</ButtonContainer>
+                </div>
+                <Link to='/details'>
+                <h3>{randomTitle}</h3>
+                </Link>
+                
+                
 
                 
-            </div>
+            </HomeGameWrapper>
      );
 }
  
 export default Home;
+
+const HomeGameWrapper = styled.div`
+{
+    padding: 20px;
+    background: var(--mainGrey);
+    color: var(--mainWhite);
+    border: 3px solid var(--lightYellow);
+    margin-top: 100px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.7), 0 6px 20px 0 rgba(0, 0, 0, 0.20);
+}
+p{
+    text-align:center;
+}
+
+h3{
+    color:var(--lightYellow);
+    text-align:center;
+    text-transform:uppercase;
+    
+}
+h3:hover, Link:hover{
+    text-decoration:none !important;
+}
+div{
+    text-align:center;
+}
+
+`
