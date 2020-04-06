c19b1eef103db907535c20cf96261d5202ae57c7
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-06 17:12:16 +0530

    Day90: Updated Context.js

--- downnine/src/context.js
+++ downnine/src/context.js
@@ -7,7 +7,9 @@ export const DataContext = createContext();
 class DataContextProvider extends Component {
     state = {
         products:[...menuItems],
-        detailProduct: detailProduct
+        detailProduct: detailProduct,
+        num:null,
+        randomTitle:null
     }
 
 
@@ -24,11 +26,32 @@ handleDetail = (id) => {
         }
     })
 }
+generateNum = () => {
+    const num = Math.floor(Math.random()*9)+1;
+    // this.setState(()=>{
+    //     return{
+    //         num:num
+    //     }
+    // })
+    return num
+}
+
+
+generateTitle = () => {
+    
+    const number = this.generateNum();
+    const item = this.showItems(number)
+    this.setState(()=>{
+        return{
+            randomTitle:item.title
+        }
+    })
+} 
 
 
 render(){
     return(
-        <DataContext.Provider value = {{...this.state, handleDetail:this.handleDetail}}>
+        <DataContext.Provider value = {{...this.state, handleDetail:this.handleDetail, generateNum:this.generateNum, generateTitle:this.generateTitle}}>
             {this.props.children}
         </DataContext.Provider>
     )
