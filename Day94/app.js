8d630134dddcabf2d7741ee7a48cdbcd5f68e6c8
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-10 16:22:09 +0530

    Day94: Routing and Template Engines

---
+++ Day94/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/contact', function(req,res){
+    res.sendFile(__dirname + '/contact.html');
+}); 
+
+app.get('/profile/:id', function(req,res){
+    res.send('This is the id of ' + req.params.id);
+});
+
+app.listen(3000);
